7f958cfc4eb8123484e9388f211225eb3b2ca12a
Tor-Naedo <minipie928@gmail.com>
2024-02-13 19:08:12 -0500

    added a camera that zooms in and out

--- Source/striVe/striVeCharacter.h
+++ Source/striVe/striVeCharacter.h
@@ -12,12 +12,12 @@ class AstriVeCharacter : public ACharacter
 	GENERATED_BODY()
 
 	/** Side view camera */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class UCameraComponent* SideViewCameraComponent;
+	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	//class UCameraComponent* SideViewCameraComponent;
 
 	/** Camera boom positioning the camera beside the character */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class USpringArmComponent* CameraBoom;
+	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	//class USpringArmComponent* CameraBoom;
 
 	void StartAttack1();
 	void StartAttack2();
@@ -41,8 +41,26 @@ protected:
 	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character")
 	//ECharacterClass characterClass;
 
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player References")
+		AstriVeCharacter* otherPlayer;
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Hitbox")
 		AActor* hurtbox;
+
+	//The caharacter's transform
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Model")
+		FTransform transform;
+
+	//The character's scale
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Model")
+		FVector scale;
+
+	//The maximum amount of distance that the players can be apart
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
+		float maxDistanceApart;
 
 	//Damage the player.
 	UFUNCTION(BlueprintCallable)
@@ -56,12 +74,15 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Health")
 		float playerHealth;
 
+	//Is the character's model flipped?
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Model")
+		bool isFlipped;
 
 public:
 	AstriVeCharacter();
 
 	/** Returns SideViewCameraComponent subobject **/
-	FORCEINLINE class UCameraComponent* GetSideViewCameraComponent() const { return SideViewCameraComponent; }
+	//FORCEINLINE class UCameraComponent* GetSideViewCameraComponent() const { return SideViewCameraComponent; }
 	/** Returns CameraBoom subobject **/
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	//FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 };
